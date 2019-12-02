e553272b44697230c8e18a95f0a2609227f5df0b
Ivan Malshchukov <nummyn0rih@gmail.com>
2019-12-02 11:20:54 +0300

    refact

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-menu.onclick = function myFunction() {
+menu.onclick = function() {
   const x = document.getElementById('myTopnav');
 
   if (x.className === 'topnav') {
@@ -6,4 +6,20 @@ menu.onclick = function myFunction() {
   } else {
     x.className = 'topnav';
   }
-}
+};
+
+form.onclick = function(event) {
+  event.preventDefault();
+
+  if (this.lastElementChild.className === "form__submitted") {
+    return;
+  }
+
+  if (event.target.className === 'submit') {
+    const p = document.createElement('p');
+    p.className = "form__submitted";
+    p.innerHTML = "Form submitted";
+
+    this.append(p);
+  }
+};
